83e30e64be35ba5076bbfcb84ee412bb34133b40
Ben Haines-Bellamy <benhaines@me.com>
2013-04-16 16:03:02 +1200

    Update class name and test method name

--- tests/AwareModelTest.php
+++ tests/AwareModelTest.php
@@ -2,14 +2,14 @@
 
 use \Mockery as m;
 
-class ModelTest extends PHPUnit_Framework_TestCase 
+class AwareModelTest extends PHPUnit_Framework_TestCase
 {
     function tearDown()
     {
         m::close();
     }
 
-    function testErrorsMethod()
+    function testgetErrorsMethod()
     {
         $model = m::mock('\Awareness\Aware[]');
         $this->assertInstanceOf('\Illuminate\Support\MessageBag', $model->getErrors());
